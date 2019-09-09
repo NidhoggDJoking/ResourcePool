fa60f56ebca853b758b5691a5f5902e5e7a80953
NidhoggDJoking <250348426@qq.com>
2019-09-09 21:28:05 +0800

    add

---
+++ TS/index.ts
@@ -0,0 +1,15 @@
+console.log("link Start!")
+
+// 第一步 tsc --init 生成tsconfig.json  修改输出路径"outDir": "./js",
+
+// 第二部 运行任务 tsc：watch  tsconfig.json 开启监听
+
+var str:String = 'joking';
+
+var edg:any = true;
+
+edg=123;
+
+function joker():void{
+
+}
---
+++ TS/js/index.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log("link Start!");
+// 第一步 tsc --init 生成tsconfig.json  修改输出路径"outDir": "./js",
+// 第二部 运行任务 tsc：watch  tsconfig.json 开启监听
+var str = 'joking';
+var edg = true;
+edg = 123;
+function joker() {
+}
